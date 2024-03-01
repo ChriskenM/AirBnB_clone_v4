30a28ba9e1675b066b4560fb415d867e2a554a15
ChriskenM <chriskenmutuma0727@gmail.com>
2024-03-01 02:23:30 -0800

    Fix file naming and structure in web_dynamic

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    const checkedBoxes = {};
+ 
+    function updateAmenities() {
+	let listOfAmenities = Object.values(checkedBoxes);
+	$(".amenities h4").text("" + listOfAmenities.join(", "))
+	console.log($(".amenities h4")[0])
+    }
+
+    $("input[type='checkbox']").change(function() {
+	if ($(this).prop("checked")) {
+	    checkedBoxes[$(this).data("id")] = $(this).data("name")
+	} else {
+	    if (checkedBoxes[$(this).data("id")]) {
+		delete checkedBoxes[$(this).data("id")]
+	    }
+	}
+    })
+    updateAmenities()
+})
